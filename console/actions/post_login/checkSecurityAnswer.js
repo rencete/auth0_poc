aef9011564dc712de1658cec05f710ac417f2f93
Terence Yu <rencete@yahoo.com>
2022-12-13 11:47:32 -0500

    Security QnA now checks sessions for step-up

--- console/actions/post_login/checkSecurityAnswer.js
+++ console/actions/post_login/checkSecurityAnswer.js
@@ -1,31 +1,39 @@
 const appUrl = 'localhost:8000';
 const redirectUrl = `http://${appUrl}/authenticate/answer_security_question`;
+const providerUrl = `http://${appUrl}/providers/security_question`;
+const promptTtl = 1000 * 60 * 60; // security answer is valid for 1 hour (session length)
 
 exports.onExecutePostLogin = async (event, api) => {
   if (event.user.user_metadata?.security_answer) {
     // A security question/answer has already been defined and can therefore be checked
-    const token = api.redirect.encodeToken({
-      secret: event.secrets.HS256_SHARED_SECRET,
-      payload: {
-        aud: event.client.client_id,
-        attempts: 0,
-      }
-    });
 
-    api.redirect.sendUserTo(redirectUrl, {
-      query: { token: token }
-    });
+    // Check if there is a valid security question session (ie. security question/answer given correctly in last X minutes)
+    const securityQuestionMethod = event.authentication?.methods.find((record) => validateSecurityQuestionRecord(record, providerUrl, promptTtl));
+    if (!securityQuestionMethod) {
+      // No valid security question/answer session, proceed to ask user
+      const token = api.redirect.encodeToken({
+        secret: event.secrets.HS256_SHARED_SECRET,
+        payload: {
+          aud: event.client.client_id,
+          attempts: 0,
+        }
+      });
+
+      api.redirect.sendUserTo(redirectUrl, {
+        query: { token: token }
+      });
+    }
   }
 
   // no security question/answer yet, just proceed to login
 };
+
 
 exports.onContinuePostLogin = async (event, api) => {
   // NOTE: using cache to store number of attempts is not a good idea
   //       the cache seems to be inconsistent when retrieving/clearing data stored
   const maxAttempts = 3
   // const cacheKey = 'securityAnswerAttempts'
-  const providerUrl = `http://${appUrl}/providers/security_question`
 
   // Note: payload will include the whole token, including mandatory claims
   const payload = api.redirect.validateToken({
@@ -55,7 +63,7 @@ exports.onContinuePostLogin = async (event, api) => {
           attempts: numAttempts,
         }
       });
-  
+
       api.redirect.sendUserTo(redirectUrl, {
         query: { token: token }
       });
@@ -77,3 +85,24 @@ exports.onContinuePostLogin = async (event, api) => {
 
   // continue to login
 };
+
+
+function validateSecurityQuestionRecord(record, url, ttl) {
+  if (!record) {
+    // No record means it isn't valid.
+    return false;
+  }
+
+  // NOTE: example shows record.url, but this is not correct
+  //       actual field is "name" and not "url"
+  if (record.name !== url) {
+    // This isn't a record of our custom method.
+    return false;
+  }
+
+  // Timestamps are rendered as ISO8601 strings.
+  const timestamp = new Date(record.timestamp);
+
+  // The record is valid if it was recorded recently enough.
+  return timestamp.valueOf() >= Date.now() - ttl;
+}
